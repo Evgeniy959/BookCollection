7bebf17429ae8b96ade17fa6b83c2d827ea847da
Pili_zr36 <77540303+Evgeniy959@users.noreply.github.com>
2023-11-15 11:53:10 +0700

    add

---
+++ BookCollection/BookCollection/ViewModel/IBookService.cs
@@ -0,0 +1,20 @@
+﻿using BookCollection.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace BookCollection.ViewModel
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBooks();
+        void AddBook(Book book);
+        void UpdateBook(Book book, int id);
+        void DeleteBook(Book book);
+        List<Book> SortAuthor();
+        List<Book> SortYear();
+    }
+}
